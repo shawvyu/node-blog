5ba91833bf603696370004cae075412ff3514ddd
hxw319726 <1140480831@qq.com>
2017-05-02 00:00:27 +0800

    51节内容分类展示

--- routers/main.js
+++ routers/main.js
@@ -15,17 +15,24 @@ router.get('/',function (req,res) {
     var data={
         userInfo:req.userInfo,
         categories:[],
+        category:req.query.category || '',
         page:Number(req.query.page || 1),
-        limit:5,
+        limit:1,
         pages:0,
         count:0,
         contents:[]
     }
+    //where
+    var where={};
+    if(data.category){
+        where.category=data.category;
+    }
+
     //读取所有的分类信息
     Category.find().then(function (categories) {
         // console.log(rs);
         data.categories=categories;
-        return Content.count().then(function (count) {
+        return Content.where(where).count().then(function (count) {
                     // console.log(count);
                     data.count=count;
                     //计算总页数
@@ -36,7 +43,7 @@ router.get('/',function (req,res) {
                     data.page=Math.max(data.page,1)
                     var skip=(data.page-1)*data.limit;
 
-                   return Content.find().limit(data.limit).skip(skip).populate(['category','user'])
+                   return Content.where(where).find().limit(data.limit).skip(skip).populate(['category','user'])
                 })
     }).then(function (contents){
         data.contents=contents;
